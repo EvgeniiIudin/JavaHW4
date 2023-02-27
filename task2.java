f3550774bccd399ab6be559b5a0ca0db8ef336bb
Evgenii Iudin <evgeniiiudin343@gmail.com>
2023-02-27 19:42:33 +0300

    add solution task2

--- task2.java
+++ task2.java
@@ -5,4 +5,39 @@
  * first() - возвращает первый элемент из очереди, не удаляя.
  */
 
- 
+ import java.util.ArrayDeque;
+
+public class task2 {
+
+    public static ArrayDeque<Integer> enqueue(ArrayDeque<Integer> linkedList, int element) {
+        linkedList.addLast(element);
+
+        return linkedList;
+    }
+
+    public static int dequeue(ArrayDeque<Integer> linkedList) {
+        int firstElement = linkedList.getFirst();
+        linkedList.pollFirst();
+
+        return firstElement;
+    }
+
+    public static int first(ArrayDeque<Integer> linkedList) {
+        return linkedList.getFirst();
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> linkedList = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            linkedList.add((int) (Math.random() * (10 - 1)));
+        }
+        int element = 77;
+        System.out.println("Очередь: " + linkedList);
+        System.out.println(
+                "Новый элемент: " + element + "\n" + "Очередь с новым элементом: " + enqueue(linkedList, element));
+        System.out.println("Удаляемый элемент: " + dequeue(linkedList));
+        System.out.println("Очередь после удаления элемента: " + linkedList);
+        System.out.println("Первый элемент: " + first(linkedList));
+    }
+
+}
